093164d8ed0e17279501a959736364989c359436
Ludvig Eriksson <ludvigeriksson@icloud.com>
2017-01-09 16:15:03 +0100

    RGB web server update

--- home/rgb_web_server/script.js
+++ home/rgb_web_server/script.js
@@ -4,4 +4,45 @@
         margin: -20,
         threshold: 3
     });
+
+    function handleEvent(mode) {
+        var data = [];
+        switch (mode) {
+            case 1:
+                // Movie mode
+                data[0] = 200;
+                data[1] = 0;
+                data[2] = 100;
+            case 2:
+                // Max brightness
+                data[0] = 255;
+                data[1] = 255;
+                data[2] = 255;
+            case 3:
+                // Match ambient
+                data[0] = 0;
+                data[1] = 255;
+                data[2] = 0;
+            case 4:
+                // Custom
+                data[0] = 0;
+                data[1] = 0;
+                data[2] = 0;
+            default:
+                return;
+        }
+        var params = [
+            'r=' + data[0],
+            'g=' + data[1],
+            'b=' + data[2]
+        ].join('&');
+        var req = new XMLHttpRequest();
+        req.open('POST', '?' + params, true);
+        req.send();
+    }
+
+    $('.comb').click(function() {
+        handleEvent(this.dataset.mode);
+    });
+
 }(jQuery));
